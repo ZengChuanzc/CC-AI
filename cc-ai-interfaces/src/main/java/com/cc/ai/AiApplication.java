3cfeb407507dc0b3f58855229d9b1937f4a754f3
zengchuan <18318610863@163.com>
2024-01-18 11:42:18 +0800

    初始化项目

---
+++ cc-ai-interfaces/src/main/java/com/cc/ai/AiApplication.java
@@ -0,0 +1,21 @@
+package com.cc.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchuan
+ * @version 1.0
+ * @date 2024/1/18 10:55
+ * @description ${NAME}
+ */
+@SpringBootApplication
+public class AiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AiApplication.class, args);
+
+    }
+
+}
